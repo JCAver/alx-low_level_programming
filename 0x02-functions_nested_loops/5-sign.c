33b9f0e6795710b10580a8df2e791bb7cb73b058
Ikyoon collins Aver <avex094@gmail.com>
2022-09-14 16:10:46 -0700

    My task-5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,16 +9,18 @@
  */
 int print_sign(int n)
 {
-	if ((n > 0))
-		return (1);
+	if (n > 0)
+	{
+	_putchar('+');
+	rturn (1);
+	}
+	else if(n < 0)
+	{
+	_putchar('-');
+	return (-1);
+	}
 	else
-		_putchar(+);
-	if ((n == 0))
-                return (0);
-        else
-                _putchar(0);
-	if ((n < 0))
-                return (-1);
-        else
-                _putchar(-);
+	{
+	_putchar('0');
+	return (0);
 }

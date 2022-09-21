27ed44fdd1f68b8e49f68983485e4ccfdaa16b98
Ikyoon collins Aver <avex094@gmail.com>
2022-09-20 20:30:26 -0700

    My pointers task

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,16 +2,17 @@
 
 /**
  * _strlen - Returns the length of a string.
- * @str: The string to get the length of.
+ * @s The string.
  *
  * Return: The length of @str.
  */
-size_t _strlen(const char *str)
+
+int _strlen(char *s)
 {
-	size_t length = 0;
+	int len = 0;
 
-	while (*str++)
-		length++;
+	while (*(s + len) != '\0')
+		len++;
 
-	return (length);
+	return (len);
 }
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,17 +2,16 @@
 
 /**
  * print_rev - Prints a string in reverse.
- * @s: The string to be printed.
+ * @s: The string to be printed in reverse.
  */
+
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int len = strlen(s);
 
-	while (s[index++])
-		len++;
+	while (len--)
+		
+		putchar(*(s + len);
 
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
-
-	_putchar('\n');
+	putchar(10);
 }

0e7fd3413295b11f4298fda72122b554b80b433e
JCAver <avex094@gmail.com>
2023-08-01 17:06:16 -0700

    Correction

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,42 +1,34 @@
 #include "lists.h"
-
 /**
- * find_listint_loop - finds the loop in a linked list.
- * @head: head of a list.
- *
- * Return: the address of the node where the loop starts.
+ * add_nodeint_end - adds node at the end
+ * @head: head of node linked list
+ * @n: value to put new node at end
+ * Return: struct
  */
-listint_t *find_listint_loop(listint_t *head)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *p2;
-	listint_t *prev;
+	listint_t *new;
+	listint_t *tmp;
 
-	p2 = head;
-	prev = head;
-	while (head && p2 && p2->next)
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
 	{
-		head = head->next;
-		p2 = p2->next->next;
-
-		if (head == p2)
-		{
-			head = prev;
-			prev =  p2;
-			while (1)
-			{
-				p2 = prev;
-				while (p2->next != head && p2->next != prev)
-				{
-					p2 = p2->next;
-				}
-				if (p2->next == head)
-					break;
-
-				head = head->next;
-			}
-			return (p2->next);
-		}
+		*head = new;
+		return (new);
 	}
+	else
+	{
+		tmp = *head;
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		tmp->next = new;
 
-	return (NULL);
+		return (new);
+	}
 }
